4927a38056148bc8d66da40d9bb018bd4de5b4ce
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2021-06-11 19:34:32 -0400

    ant design y formato de less

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "./App.less";
+import { Button, DatePicker } from "antd";
 function App() {
-  return <div>POS ALMACENES</div>;
+  return (
+    <div>
+      <Button danger type="primary">
+        POS ALMACENES
+      </Button>
+      <DatePicker />
+    </div>
+  );
 }
 
 export default App;
