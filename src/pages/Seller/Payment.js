4384de409d69b62d62b69851033aef3a7a3d0598
nFrosto <nicolas.ortizc@mail.udp.cl>
2021-06-14 18:09:46 -0400

    boton volver payment

--- src/pages/Seller/Payment.js
+++ src/pages/Seller/Payment.js
@@ -3,35 +3,13 @@ import { Row, Col, Divider} from 'antd';
 import {CreditCardFilled } from "@ant-design/icons"
 import {DollarCircleFilled } from "@ant-design/icons"
 import {BankFilled} from "@ant-design/icons"
-import { List, Avatar } from 'antd';
+import { List, Avatar, Button, Checkbox  } from 'antd';
 import { CartContext } from "../../context/CartContext";
-const data = [
-  {
-    title: 'Queso',
-    value: 2500,
-    cantidad: 4,
-  },
-  {
-    title: 'Jamon',
-    value: 3000,
-    cantidad: 2,
-  },
-  {
-    title: 'Salmon',
-    value: 4500,
-    cantidad: 3,
-  },
-  {
-    title: 'Cucarachas',
-    value: 4000,
-    cantidad: 5,
-  },
-];
+import {LeftCircleTwoTone} from "@ant-design/icons"
 
-
-const style = {padding: '8px 0' };
-
-
+/*style de padding solo afecta los gutterrow 2 y 3 */
+const style = {padding: '31px 0'};
+const style2 = {floar: 'right'};
 
 const Payment = () => {
   const [cart, setCart] = useContext(CartContext)
@@ -44,12 +22,14 @@ const Payment = () => {
   return(
     <div>
       <>
-      <Row gutter={16}>
-      <Col className="gutter-row" span={10}>
+      <Row gutter={100}>
+      <Col className="gutter-row" span={11}>
+      <Button icon = {<LeftCircleTwoTone />} type="primary" shape="round" size = "Large" onClick={() => {window.location.replace("/pos")}} >Volver</Button>
       <Divider >Productos</Divider>
   <List 
     itemLayout="horizontal"
-    
+    footer={<div>Total</div>}
+    footer={<div style = {style2}>Total</div>}
     dataSource={cart.productList}
     renderItem={item => (
       <List.Item>
@@ -57,15 +37,18 @@ const Payment = () => {
           /*avatar={<Avatar src="/uploads/perro_729fefb8c4.png" />}*/
           title={item.productName}
           description={<div>{item.quantity} unidades a ${item.unitPrice}/unidad   </div>}
+         
+     
         />
          <div>{item.totalPrice}</div>
          {console.log(cart)}
       </List.Item>
     )}
-  />,
+  />
 
       </Col>
-      <Col className="gutter-row" span={7}>
+      
+      <Col className="gutter-row" span={5}>
         <div style={style}>
         <Divider >Medio de pago</Divider>
         <div style={style}><h3> <DollarCircleFilled /> Efectivo</h3></div>       
@@ -73,13 +56,13 @@ const Payment = () => {
         <div style={style}><h3><BankFilled /> Debito </h3></div>
         </div>
       </Col>
-      <Col className="gutter-row" span={5}>
+      <Col className="gutter-row" span={4}>
         <div style={style}>col-6 
         
         </div>
       </Col>
     </Row>
-      </>,
+      </>
     </div>
   );
 };
