e137b0659113a946b8e3959bc4acc2aedf33405f
nFrosto <nicolas.ortizc@mail.udp.cl>
2021-06-15 20:46:07 -0400

    numpad

--- src/pages/Seller/Payment.js
+++ src/pages/Seller/Payment.js
@@ -76,34 +76,39 @@ const Payment = () => {
               </div>
             </div>
             <Divider >Ingrese monto para calculo de vuelto</Divider>
+            
             <div class="btn-group-vertical ml-4 mt-4" role="group" aria-label="Basic example">
               <div class="btn-group">
-                <input class="text-center form-control-lg mb-2" id="code" ></input>
+                <input style = {{width: 300,  height: 50}} type= "int" class="text-center form-control-lg mb-1" id="code" ></input>
               </div>
               <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '1' }}>1</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '2' }}>2</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '3' }}>3</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '1' }}>1</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '2' }}>2</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '3' }}>3</button>
               </div>
               <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '4' }}>4</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '5' }}>5</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '6' }}>6</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '4' }}>4</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '5' }}>5</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '6' }}>6</button>
               </div>
               <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '7' }}>7</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '8' }}>8</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '9' }}>9</button>
+                <button type="button"style = {{width: 100,  height: 50, background: "#FFF0F5"}} class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '7' }}>7</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '8' }}>8</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '9' }}>9</button>
               </div>
               <div class="btn-group">
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value.slice(0, -1)}}>&lt;</button>
-                <button type="button" class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '0' }}>0</button>
-                <button type="button" class="btn btn-primary py-3" onclick="">Go</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value.slice(0, -1)}}>&lt;</button>
+                <button type="button"style = {{width: 100,  height: 50, background: "#FFF0F5"}} class="btn btn-outline-secondary py-3" onClick={() => { document.getElementById('code').value=document.getElementById('code').value + '0' }}>0</button>
+                <button type="button" style = {{width: 100,  height: 50, background: "#FFF0F5"}}class="btn btn-primary py-3" onClick={() => { document.getElementById('code').value='' }}>Limpiar</button>
               </div>
             </div>
           </Col>
           <Col className="gutter-row" span={4}>
-            <div style={style}>col-6
+            <div style={style}>
+
+            <Divider >Vuelto</Divider>
+            
+            
 
             </div>
           </Col>
